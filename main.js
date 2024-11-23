f5bc6173c2ce426bc4cc16ea4b7f20a6069226bc
devharby <139724857+devharby@users.noreply.github.com>
2024-11-24 01:13:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let answer = document.getElementById("answer");
 let show = document.getElementById("show");
 let hide = document.getElementById("hide");
+let faq = document.getElementById("faq");
 
 show.onclick = function show() {
-  answer.style.display = "block";
+  answer.style.transform = "scale(1)";
+  answer.style.display = "block"
 };
 
 hide.onclick = function hide() {
-  answer.style.display = "none";
+  answer.style.transform = "scale(0)";
+  faq.style.height = "auto";
 };
